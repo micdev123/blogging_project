f1dd1e13751333cd5c54bb1f14300f9c04b77a4b
Michael L Bangura <michlawbang123@gmail.com>
2022-09-01 23:13:01 -1200

    done

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BsTwitter, BsInstagram, BsGithub } from 'react-icons/bs'
-import { FaFacebook } from 'react-icons/fa'
+import { AiFillLinkedin } from 'react-icons/ai'
 
 import './footer.css'
 
@@ -9,10 +9,18 @@ export const Footer = () => {
         <div className='Footer_Component Dark_Mode_Background'>
             <p className='Dark_Mode_P'>&copy;copyrights :: Mic__Dev.com ~ Blogging.com</p>
             <div className='Footer_Socials'>
-                <BsGithub className='icon' />
-                <BsTwitter className='icon' />
-                <BsInstagram className='icon' />
-                <FaFacebook className='icon'/>
+                <a target="_blank" rel="noreferrer" href="https://github.com/micdev123">
+                    <BsGithub className='icon' />
+                </a>
+                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/michael-l-bangura-2a52a724a/">
+                    <AiFillLinkedin className='icon' />
+                </a>
+                <a target="_blank" rel="noreferrer" href="https://twitter.com/Mic__Dev">
+                    <BsTwitter className='icon' />
+                </a>
+                <a target="_blank" rel="noreferrer" href="https://instagram.com/mic__dev">
+                    <BsInstagram className='icon' />
+                </a>
             </div>
         </div>
     )
